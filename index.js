c14c9a785ad7ded98b99c35f92943a35267dac6f
HareemShaikh01 <hareemshaikh2006@gmail.com>
2025-02-09 14:48:26 +0500

    234

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import notesrouter from "./routes/notes.js"
 import cors from "cors"
 
 const app = express();
-app.use(cors({ origin: 'https://frontend-inotebook.vercel.app' }));
+app.use(cors({
+    origin: "https://frontend-inotebook.vercel.app",
+    credentials: true, // Allow cookies & authentication headers
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 
 
